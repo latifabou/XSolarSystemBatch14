83171b5437dc0926e1f3fa88715a6361ddbf6c91
latifabou <Abdellah.Bouzid@hotmail.com>
2022-12-18 18:56:09 -0500

    the basics of solar system are ready

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem xx = new SolarSystem();
+        xx.sun = 1;
+        xx.moon = 1;
+        xx.planets = 2;
+    }
+}
