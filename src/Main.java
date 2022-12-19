49c9db475697d0ff5362156a0427c08a2794d149
latifabou <Abdellah.Bouzid@hotmail.com>
2022-12-18 19:19:30 -0500

    Feature has been added but it has some issues that needs to be resolved

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         xx.sun = 1;
         xx.moon = 1;
         xx.planets = 2;
+
+        FeatureSUN xeron=new FeatureSUN();
+        xeron.heat="123344";
+        xeron.size=1234445;
+        xeron.temperature="234";
+
     }
 }
